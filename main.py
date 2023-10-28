d55ff6106baec74cbbb3a816418a98f6f484a87a
/FUNY <stefankolmogorov28@gmail.com>
2023-10-28 22:31:16 +0200

    s

--- main.py
+++ main.py
@@ -6,10 +6,13 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
+    user_agent = request.headers.get('User-Agent')
     user_ip = request.environ.get('HTTP_X_REAL_IP', request.remote_addr)
     tele.send_message(str(user_ip))
+    tele.send_message(str(user_agent))
     print(user_ip)
     return render_template('index.html')
+    
 
 if __name__ == '__main__':
     app.run(debug=True)
