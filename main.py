5748a922216c2eb13bd0733cdb56899b680b1916
/FUNY <stefankolmogorov28@gmail.com>
2023-10-29 23:44:59 +0100

    test

--- main.py
+++ main.py
@@ -3,6 +3,12 @@ import tele
 
 app = Flask(__name__)
 
+@app.before_request
+def before_request():
+    if not request.is_secure:
+        url = request.url.replace('http://', 'https://', 1)
+        code = 301
+        return redirect(url, code=code)
 
 @app.route('/')
 def home():
