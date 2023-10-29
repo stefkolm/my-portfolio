0fb3bd0045b7ed326e13e277f71b6c029fdf0072
/FUNY <stefankolmogorov28@gmail.com>
2023-10-29 18:19:11 +0100

    gf

--- main.py
+++ main.py
@@ -6,9 +6,6 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    user_ip = request.environ.get('HTTP_X_REAL_IP', request.remote_addr)
-    tele.send_message(str(f"User from: {user_ip}"))
-    print(user_ip)
     return render_template('index.html')
     
 
