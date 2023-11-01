bdb5e39fc8ac288336e369faf47bd99e22bcf847
/FUNY <stefankolmogorov28@gmail.com>
2023-11-01 12:52:39 +0100

    https

--- main.py
+++ main.py
@@ -3,7 +3,11 @@ import tele
 
 app = Flask(__name__)
 
-
+@app.before_request
+def before_request():
+    if not request.is_secure and request.headers.get('X-Forwarded-Proto') != 'https':
+        url = "https://www.stefkolm.com"
+        return redirect(url, code=301)
 
 @app.route('/')
 def home():
